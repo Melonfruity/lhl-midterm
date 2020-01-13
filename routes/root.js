74277dc92db8f97087e425eae94f9ae7cbe47316
melonfruity <eric.luo24@gmail.com>
2020-01-13 18:45:58 -0500

    changed some layouts

--- routes/root.js
+++ routes/root.js
@@ -7,6 +7,11 @@ const rootRouterWrapper = (db) => {
     res.render('index');
   });
 
+  // renders room page
+  rootRouter.get('/room', (req, res) => {
+    res.render('room');
+  });
+
   // renders stats page
   rootRouter.get('/stats', (req, res) => {
     res.render('stats');
