a59e77cb5506bf6ac9ff51723e5c4a4d32d00ccb
melonfruity <eric.luo24@gmail.com>
2020-01-13 12:31:03 -0500

    adding template to games

--- routes/games.js
+++ routes/games.js
@@ -1,27 +1,68 @@
 const gamesRouter = require('express').Router();
 
 const gamesRouterWrapped = (db) => {
-  
-  // test route
-  gamesRouter.get('/', (req, res) => {
-    // TEST API QUERY
 
+  // Have frontend pass player_id and game_id
+  
+  // game_states update
+  gamesRouter.post('/state', (req, res) => {
+    const { player_id, game_id } = req;
+    
+    // make a call to dealer
+
+    // change to update
     const queryString = `
-      SELECT * FROM users;
+      SELECT * FROM game_states;
     `
     db
       .query(queryString)
       .then((data) => res.status(200).json(data.rows))
       .catch((err) => res.status(400).json(err.stack));
-  });
-  
-  // game update
-  gamesRouter.post('/games/:id', (req, res) => {
 
   });
 
-  // game update
-  gamesRouter.get('/games/:id', (req, res) => {
+  // game_states get
+  gamesRouter.get('/state', (req, res) => {
+    const { player_id, game_id } = req;
+
+    // change as necessary
+    const queryString = `
+      SELECT * FROM game_states;
+    `
+    db
+      .query(queryString)
+      .then((data) => res.status(200).json(data.rows))
+      .catch((err) => res.status(400).json(err.stack));
+
+  });
+
+  // player_hand update
+  gamesRouter.post('/hand', (req, res) => {
+    const { player_id, game_id } = req;
+
+    // change as necessary
+    const queryString = `
+      SELECT * FROM game_states;
+    `
+    db
+      .query(queryString)
+      .then((data) => res.status(200).json(data.rows))
+      .catch((err) => res.status(400).json(err.stack));
+
+  });
+
+    // player_hand get
+  gamesRouter.get('/hand', (req, res) => {
+    const { player_id, game_id } = req;
+
+    // change as necessary
+    const queryString = `
+      SELECT * FROM game_states;
+    `
+    db
+      .query(queryString)
+      .then((data) => res.status(200).json(data.rows))
+      .catch((err) => res.status(400).json(err.stack));
 
   });
 
