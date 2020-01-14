b7f608876cbb0c54496cd75ada08b19aff3745b8
Susan Zhang <susanwenzhang@gmail.com>
2020-01-14 17:03:56 +0000

    aligned seeds to updated database schema

---
+++ db/seeds/03_game_types.sql
@@ -0,0 +1,6 @@
+-- GOPS
+INSERT INTO games (name, description, tutorial, min_players, max_players)
+VALUES ('GOPS', 'Goofspiel, also known as The Game of Pure Strategy or GOPS, is a card game for two or more players. It was invented by Merrill Flood while at Princeton University in the 1930s,[1] and Alex Randolph describes a similar game as having been popular with the 5th Indian Army during the Second World War.', 'Goofspiel is played using cards from a standard deck of cards, and is typically a two-player game, although more players are possible. Each suit is ranked A (low), 2, ..., 10, J, Q, K (high).', 2, 3);
+
+INSERT INTO games (name, description, tutorial, min_players, max_players)
+VALUES ('Volatile Felines', 'Volatile Felines is a spoof of Exploding Kittens, a card game designed by Elan Lee, Matthew Inman from the comics site The Oatmeal, and Shane Small. ', 'All cards are put into a deck, save for the Defuse and Exploding Kitten cards. The deck is shuffled and each player draws 7 cards and takes a Defuse card. ', 2, 3);
---
+++ db/seeds/09_player_hands_seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO player_hands (game_state_id, user_id, suit, score, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9, card_10, card_11, card_12, card_13)
+VALUES (1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1);
+INSERT INTO player_hands (game_state_id, user_id, suit, score, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9, card_10, card_11, card_12, card_13)
+VALUES (1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO player_hands (game_state_id, user_id, suit, score, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9, card_10, card_11, card_12, card_13)
+VALUES (1, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1);
+INSERT INTO player_hands (game_state_id, user_id, suit, score, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9, card_10, card_11, card_12, card_13)
+VALUES (2, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1);
