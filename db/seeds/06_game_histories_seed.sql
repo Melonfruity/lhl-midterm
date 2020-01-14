4786a1dfa13635efc89258d8e3b4c01f25bd00ad
Susan Zhang <susanwenzhang@gmail.com>
2020-01-14 22:13:45 +0000

    setting back to seeds made earlier

---
+++ db/seeds/06_game_histories_seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO game_histories (room_id, game_id, winner) VALUES (1, 1, 1);
+INSERT INTO game_histories (room_id, game_id, winner) VALUES (3, 1, 2);
+INSERT INTO game_histories (room_id, game_id, winner) VALUES (1, 1, 3);
+INSERT INTO game_histories (room_id, game_id, winner) VALUES (2, 2, 4);
+INSERT INTO game_histories (room_id, game_id, winner) VALUES (1, 2, 5);
+INSERT INTO game_histories (room_id, game_id, winner) VALUES (3, 2, 6);
