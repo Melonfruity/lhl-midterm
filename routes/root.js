e1893249cff37ee890b1fa104721be83134dcada
melonfruity <eric.luo24@gmail.com>
2020-01-14 12:03:25 -0500

    doing some css

--- routes/root.js
+++ routes/root.js
@@ -16,17 +16,13 @@ const rootRouterWrapper = (db) => {
     res.render('index', templateVars);
   });
 
-  // renders room page
-  rootRouter.get('/room', (req, res) => {
-    res.render('room');
-  });
-
   // renders stats page
   rootRouter.get('/stats', (req, res) => {
     const templateVars = {user: req.session ? req.session.userID: null}
     res.render('stats', templateVars);
   });
-
+  
+  // renders room page
   rootRouter.get('/rooms', (req, res) => {
     const templateVars = {user: req.session ? req.session.userID: null}
     res.render('rooms', templateVars);
