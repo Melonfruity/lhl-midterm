37abfef83cbd93692e599ecb02a667df89e7a301
Susan Zhang <susanwenzhang@gmail.com>
2020-01-14 20:28:07 +0000

    setup profile page layout and linked database to populate information

--- routes/root.js
+++ routes/root.js
@@ -6,22 +6,39 @@ const rootRouterWrapper = (db) => {
   // renders lobby page
   rootRouter.get('/', (req, res) => {
     databaseHelper.getAllGames()
-    .then((gamesData) => {
-      const templateVars = {user: req.session ? req.session.userID : null, gamesData}
-      res.render('index', templateVars);
-    })
-    .catch((err) => res.status(400).json(err.stack));
+      .then((gamesData) => {
+        const templateVars = { user: req.session ? req.session.userID : null, gamesData }
+        res.render('index', templateVars);
+      })
+      .catch((err) => res.status(400).json(err.stack));
+  });
+
+  // renders profile page
+  rootRouter.get('/profile', async (req, res) => {
+    const userData = await databaseHelper.getUserDetailsWithId(1) // temporary, pull actual user id from cookie
+      .then((userData) => {
+        console.log("USERDATA", userData)
+        return userData;
+      })
+      .catch((err) => res.status(400).json(err.stack));
+    const gameData = await databaseHelper.getGamesWonWithUserId(1) // temporary, pull actual user id from cookie
+      .then((gameData) => {
+        console.log("GAMEDATA", gameData)
+        return gameData;
+      })
+    const templateVars = { user: req.session ? req.session.userID : null, userData, gameData }
+    res.render('profile', templateVars);
   });
 
   // renders stats page
   rootRouter.get('/stats', (req, res) => {
-    const templateVars = {user: req.session ? req.session.userID: null}
+    const templateVars = { user: req.session ? req.session.userID : null }
     res.render('stats', templateVars);
   });
-  
+
   // renders room page
   rootRouter.get('/rooms', (req, res) => {
-    const templateVars = {user: req.session ? req.session.userID: null}
+    const templateVars = { user: req.session ? req.session.userID : null }
     res.render('rooms', templateVars);
   });
 
