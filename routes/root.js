a1c34a86e6d1d865539ed225916cd35b60bd8d6b
Susan Zhang <susanwenzhang@gmail.com>
2020-01-15 20:16:18 +0000

    fixed page load set interval issue

--- routes/root.js
+++ routes/root.js
@@ -55,7 +55,8 @@ const rootRouterWrapper = (db) => {
   });
 
   rootRouter.get('/game', (req, res) => {
-    res.render('game');
+    const templateVars = { user: req.session ? req.session.userID : null }
+    res.render('game', templateVars);
   })
 
 
