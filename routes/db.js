c989567f73ce519c1d4ffc713263d30140afd0ee
melonfruity <eric.luo24@gmail.com>
2020-01-16 12:24:05 -0500

    done the refresh feature for game list

--- routes/db.js
+++ routes/db.js
@@ -7,13 +7,15 @@ const dbRouterWrapper = (db) => {
     const { game_id } = req.query;
     const queryParams = [game_id];
     const queryString = `
-      SELECT * FROM rooms
-      WHERE rooms.game_id = $1;
+      SELECT rooms.id as room_id, count(*), rooms.game_started as game_started 
+        FROM rooms
+        JOIN room_users ON room_users.room_id = rooms.id 
+        WHERE rooms.game_id = $1 
+        GROUP BY rooms.id;
     `
     db
       .query(queryString, queryParams)
       .then((data) => {
-        console.log(data.rows)
         res.status(200).json(data.rows)})
       .catch((err) => res.status(400).json(err.stack));
 
