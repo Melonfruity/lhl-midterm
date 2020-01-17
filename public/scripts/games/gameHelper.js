a19a5575ae8cf75987c385293a98dd2e13823de9
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-17 16:53:30 +0000

    last commit

--- public/scripts/games/gameHelper.js
+++ public/scripts/games/gameHelper.js
@@ -1,4 +1,28 @@
+const getNewDealerAndDisPlay = function(room_id) {
+  $.ajax({
+    method: "post",
+    url: `/api/games/deal/?room_id=${room_id}`,
+    success:
+      getGameState(room_id)
+        .then(data => {
+          console.log('data',data)
+          dealer_card = data.dealer_card;
+          game_state_id = data.id;
+          loadPage(dealer_card);
+        })
 
-const checkIfRoundFinish = function () {
-  
+
+
+  });
+}
+
+
+
+$(function() {
+  console.log('hello');
+  //initialize game
+  const pageURL = $(location).attr("href");
+  room_id = roomIdFromUrl(pageURL);
+  getNewDealerAndDisPlay(room_id);
+
 }
